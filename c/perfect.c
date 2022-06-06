bd5f74e174d1f667b25caf5df0a564d98c74c8d3
Duttaluru sasikumar <sasi54563@gmail.com>
2022-06-06 05:56:01 +0530

    Create perfect.c

---
+++ c/perfect.c
@@ -0,0 +1,23 @@
+//Perfect Number Program in C
+
+/*Create a C Program to find the perfect number using while loop*/  
+#include<stdio.h>  
+#include<conio.h>  
+void main()  
+{  
+int i = 1, num, Sum = 0;  
+printf(" Enter any number to check Perfect Number \n");  
+scanf("%d", &num);  
+  
+while(i < num )  
+                     {  
+                               if(num % i == 0)  
+                               Sum = Sum + i;  
+                               i++;  
+                     }  
+           if(Sum == num)  
+                  printf("\n %d is Perfect Number", num);  
+           else  
+           printf("\n %d is not a Perfect Number", num);  
+getch();  
+}   
