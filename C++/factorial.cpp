4a4c39cd2996d8af980c7c058d3b55d07be55b45
Duttaluru sasikumar <sasi54563@gmail.com>
2022-06-10 12:01:00 +0530

    Create factorial.cpp

---
+++ C++/factorial.cpp
@@ -0,0 +1,18 @@
+//Factorial program in C++
+//Factorial Program using Loop
+
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+   int i,fact=1,number;    
+  cout<<"Enter any Number: ";    
+ cin>>number;    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  cout<<"Factorial of " <<number<<" is: "<<fact<<endl;  
+  return 0;  
+}  
+
