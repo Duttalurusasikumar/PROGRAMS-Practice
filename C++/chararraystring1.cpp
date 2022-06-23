fc3aab65977babf6a5f2f1e3abafd83d79f4104c
Duttaluru sasikumar <sasi54563@gmail.com>
2022-06-23 06:21:13 +0530

    Create chararraystring1.cpp

---
+++ C++/chararraystring1.cpp
@@ -0,0 +1,19 @@
+//We can convert character to a string using 'for' a loop by 
+
+
+#include <iostream>  
+using namespace std;  
+int main()   
+{  
+    char char_array[] = {'J','A','V','A','T','P','O','I','N','T'};  
+    int array_size = sizeof(char_array) /  
+    sizeof(char);  
+    string j = "";  
+    int i;  
+    for(i = 0; i < array_size; i++)  
+   {  
+    /** retrieving and merging the value of character array on position 'i'*/    
+    j = j + char_array[i];  
+    }  
+    cout << j <<endl;  
+}   
