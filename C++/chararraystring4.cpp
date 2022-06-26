7c5ad10a089f5dc6a9800f1f3362140d2e3d2039
Duttaluru sasikumar <sasi54563@gmail.com>
2022-06-26 07:12:35 +0530

    Create chararraystring4.cpp

---
+++ C++/chararraystring4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  
+  using namespace std;  
+   int main()  
+{  
+   char char_array[] = {'J','A','V','A','T','P','O','I','N','T'};  
+   string j = char_array;  
+   cout << j <<endl;   
+}    
