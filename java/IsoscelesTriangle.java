c9cd214fe04c34710f5807c02cc9f3d9c1334529
Duttaluru sasikumar <sasi54563@gmail.com>
2022-07-16 06:14:43 +0530

    Create IsoscelesTriangle.java

---
+++ java/IsoscelesTriangle.java
@@ -0,0 +1,24 @@
+//Java Program To Find Area Of Isosceles Triangle
+
+
+import java.util.Scanner;
+class AreaOfIsoscelesTriangle 
+{
+   public static void main(String args[]) 
+    {   
+       
+         Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the length of same sided");
+         
+         double a= s.nextDouble();
+         
+         System.out.println("Enter the side2 of the Triangle");
+         
+         double b= s.nextDouble();
+
+         double  area=(b/4)*Math.sqrt((4*a*a)-(b*b));
+      
+         System.out.println("Area of Isosceles Triangle is: " + area);
+   }
+}
