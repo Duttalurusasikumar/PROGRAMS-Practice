99898df9560b83e01df4c8d6039ba3f402b3b0f1
Duttaluru sasikumar <sasi54563@gmail.com>
2022-07-17 07:56:53 +0530

    Create parallelogram.java

---
+++ java/parallelogram.java
@@ -0,0 +1,19 @@
+//Java Program To Find Area of Parallelogram – Programs
+
+import java.util.Scanner;
+class AreaOfParallelogram 
+{
+   public static void main(String args[]) 
+    {   
+       
+      Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the height:");
+         double d1= s.nextDouble();
+        System.out.println("Enter the breadth:");
+         double d2= s.nextDouble()
+          
+          double  area=(d1*d2) ;
+      System.out.println("Area of Parallelogram is: " + area);      
+   }
+}
