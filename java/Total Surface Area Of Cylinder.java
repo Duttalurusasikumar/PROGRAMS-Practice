5e43c677243a275a918e8dcc9d02f7ab3fb26903
Duttaluru sasikumar <sasi54563@gmail.com>
2022-07-31 06:16:06 +0530

    Create Total Surface Area Of Cylinder.java

---
+++ java/Total Surface Area Of Cylinder.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class TotalSurfaceAreaOfCylinder 
+{
+   public static void main(String args[]) 
+   {   
+       
+
+        
+          
+     		Scanner s= new Scanner(System.in);
+        
+         	System.out.println("Enter the radius:");
+         	
+      		double r=s.nextDouble();
+         	
+		System.out.println("Enter the height:");
+         	
+		double h=s.nextDouble();
+ 
+            	double  tsa=((2*22*r)/7)*(r+h);
+
+            	System.out.println("TotalSurfaceArea Of Cylinder is:  " + tsa);
+       
+
+     }
+}
