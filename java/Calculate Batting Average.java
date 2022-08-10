9d6e90bb2d7e3a156d47856e8b961d624924f592
Duttaluru sasikumar <sasi54563@gmail.com>
2022-08-10 06:04:29 +0530

    Create Calculate Batting Average.java

---
+++ java/Calculate Batting Average.java
@@ -0,0 +1,14 @@
+class BattingAverage
+{
+  public static void main(String arg[])
+  {
+     int Matches=5,totalruns=200,innings=5,notout=1;
+
+     double avg;
+
+     avg=totalruns/(innings-notout);
+
+     System.out.println("batting average="+avg);
+
+  }
+}
