ba13471a274398fa564ff77029274e86c944ccdc
Duttaluru sasikumar <sasi54563@gmail.com>
2022-10-06 06:20:35 +0530

    Create Example-27.js

---
+++ Java script/Example-27.js
@@ -0,0 +1,22 @@
+//JavaScript: Check whether a string starts with 'Java' and false otherwise
+
+function start_spec_str(str)
+{
+  if (str.length < 4)
+  {
+    return false;
+  }
+  front = str.substring(0, 4);
+  if (front == 'Java') 
+  {
+    return true;
+  }
+   else 
+   {
+    return false;
+  }
+}
+
+console.log(start_spec_str("JavaScript"));
+console.log(start_spec_str("Java"));
+console.log(start_spec_str("Python"));
