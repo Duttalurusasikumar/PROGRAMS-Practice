299d72295d75264e6c97da77f037fc64347a6891
Duttaluru sasikumar <sasi54563@gmail.com>
2022-10-19 07:13:09 +0530

    Create Example-40.js

---
+++ Java script/Example-40.js
@@ -0,0 +1,20 @@
+//JavaScript: Check from two given integers whether one of them is 8 or their sum or difference is 8
+
+
+function check8(x, y) {
+  if (x == 8 || y == 8) {
+    return true;
+  }
+
+  if (x + y == 8 || Math.abs(x - y) == 8)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(check8(7, 8));
+console.log(check8(16, 8));
+console.log(check8(24, 32));
+console.log(check8(17, 18));
