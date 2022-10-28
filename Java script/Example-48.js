db12109ab1419cff96aa00e13f011301519b104c
Duttaluru sasikumar <sasi54563@gmail.com>
2022-10-28 07:06:30 +0530

    Create Example-48.js

---
+++ Java script/Example-48.js
@@ -0,0 +1,8 @@
+function string_reverse(str) 
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("w3resource"));
+console.log(string_reverse("www"));
+console.log(string_reverse("JavaScript"));
