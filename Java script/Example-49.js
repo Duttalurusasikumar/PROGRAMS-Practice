a3f3863f887658669f6946a7267aea37c6082f51
Duttaluru sasikumar <sasi54563@gmail.com>
2022-10-29 10:03:30 +0530

    Create Example-49.js

---
+++ Java script/Example-49.js
@@ -0,0 +1,33 @@
+//JavaScript: Replace every character in a given string with the character following it in the alphabet
+
+
+function string_reverse(str) 
+function LetterChanges(text) {
+//https://goo.gl/R8gn7u
+var s = text.split('');
+for (var i = 0; i < s.length; i++) {
+// Caesar cipher
+switch(s[i]) {
+case ' ':
+break;
+case 'z':
+s[i] = 'a';
+break;
+case 'Z': 
+s[i] = 'A';
+break;
+default:
+s[i] = String.fromCharCode(1 + s[i].charCodeAt(0));
+}
+
+// Upper-case vowels
+switch(s[i]) {
+case 'a': case 'e': case 'i': case 'o': case 'u':
+s[i] = s[i].toUpperCase();
+}
+}
+return s.join('');
+}
+console.log(LetterChanges("PYTHON"));
+console.log(LetterChanges("W3R"));
+console.log(LetterChanges("php"));
