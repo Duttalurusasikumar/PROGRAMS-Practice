6548f0e24657e05adc77dbc0f88a2a88dfb9bc2c
Duttaluru sasikumar <sasi54563@gmail.com>
2022-10-31 06:41:34 +0530

    Create Example-51.js

---
+++ Java script/Example-51.js
@@ -0,0 +1,10 @@
+function time_convert(num)
+ { 
+  var hours = Math.floor(num / 60);  
+  var minutes = num % 60;
+  return hours + ":" + minutes;         
+}
+
+console.log(time_convert(71));
+console.log(time_convert(450));
+console.log(time_convert(1441));
