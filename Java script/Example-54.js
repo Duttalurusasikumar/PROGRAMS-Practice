962d77bc8e1eaf83afb2ef7b34b35063fb637b31
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-03 07:00:47 +0530

    Create Example-54.js

---
+++ Java script/Example-54.js
@@ -0,0 +1,11 @@
+//JavaScript: Count the number of vowels in a given string
+
+
+function vowel_Count(str)
+{ 
+
+  return str.replace(/[^aeiou]/g, "").length; 
+}
+
+console.log(vowel_Count("Python"));
+console.log(vowel_Count("w3resource.com"));
