98aac1313702fff4d652d971960cde50f23bbd7c
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-06 06:28:23 +0530

    Create Example-57.js

---
+++ Java script/Example-57.js
@@ -0,0 +1,13 @@
+//JavaScript: Create a new string of specified copies of a given string
+
+
+function string_copies (str, n) 
+{
+  if (n < 0)
+    return false;
+  else
+  return str.repeat(n);
+}
+console.log(string_copies("abc", 5));
+console.log(string_copies("abc", 0));
+console.log(string_copies("abc", -2));
