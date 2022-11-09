6750cb6f3f64a98425aa2107798916dd0ba9ca75
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-09 08:28:14 +0530

    Create Example-60.js

---
+++ Java script/Example-60.js
@@ -0,0 +1,6 @@
+function without_first_end(str) {
+  return str.substring(1, str.length - 1);
+}
+console.log(without_first_end('JavaScript'));
+console.log(without_first_end('JS'));
+console.log(without_first_end('PHP'));
