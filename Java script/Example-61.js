18452da3c56219cc5e4ed8988cf4f0e5c24282ae
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-10 06:27:46 +0530

    Create Example-61.js

---
+++ Java script/Example-61.js
@@ -0,0 +1,11 @@
+//JavaScript: Concatenate two strings except their first character
+
+function concatenate(str1, str2) {
+  str1 = str1.substring(1, str1.length);
+  str2 = str2.substring(1, str2.length);
+  return str1 + str2;
+}
+
+console.log(concatenate("PHP","JS"));
+console.log(concatenate("A","B"));
+console.log(concatenate("AA","BB"));
