be62605cf9ccf44c929f2d82def9d2ac462e0564
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-11 08:24:11 +0530

    Create Example-62.js

---
+++ Java script/Example-62.js
@@ -0,0 +1,12 @@
+//JavaScript: Move last three character to the start of a specified string
+
+function right_three(str) {
+     if (str.length > 1)
+       {
+         return str.slice(-3) + str.slice(0, -3);
+       }
+  return str;
+}
+console.log(right_three("Python"));
+console.log(right_three("JavaScript"));
+console.log(right_three("Hi"));
