7f9e3d8decab79ae377996c4cfd7ee82940c1cb6
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-17 08:23:56 +0530

    Create Example-68.js

---
+++ Java script/Example-68.js
@@ -0,0 +1,11 @@
+//JavaScript: Create a new string taking the first and last n characters from a given string
+
+function two_string(str, n)
+  {
+    first_part = str.substring(0, n);
+    last_part = str.substring(str.length - n);
+    return first_part + last_part;
+ }
+
+console.log(two_string("JavaScript", 2));
+console.log(two_string("JavaScript", 3));
