384daf33f2f09883a4274c641c1a522382c25e6b
Duttaluru sasikumar <sasi54563@gmail.com>
2022-11-18 06:38:29 +0530

    Create Example-69.js

---
+++ Java script/Example-69.js
@@ -0,0 +1,10 @@
+//JavaScript: Compute the sum of three elements of a given array of integers of length 3
+
+function sum_three(nums)
+{
+  return nums[0] + nums[1] + nums[2];
+}
+
+console.log(sum_three([10, 32, 20]));  
+console.log(sum_three([5, 7, 9])); 
+console.log(sum_three([0, 8, -11]));
