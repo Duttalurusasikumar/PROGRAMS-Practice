632d85a30ed1e2bedc957b9eb283e199c5bc7503
Duttaluru sasikumar <sasi54563@gmail.com>
2022-12-05 07:13:37 +0530

    Create example-86.js

---
+++ Java script/example-86.js
@@ -0,0 +1,19 @@
+//JavaScript: Find the types of a specified angle
+
+function angle_Type(angle) {
+  if(angle < 90) {
+    return "Acute angle.";
+  }
+  if(angle === 90) {
+    return "Right angle.";
+  }
+  if(angle < 180) {
+    return "Obtuse angle.";
+  }
+  return "Straight angle.";
+}
+
+console.log(angle_Type(47))
+console.log(angle_Type(90))
+console.log(angle_Type(145))
+console.log(angle_Type(180))
