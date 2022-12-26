317e733041d935b5b057f79d8fea4eadaab0c354
Duttaluru sasikumar <sasi54563@gmail.com>
2022-12-26 09:08:36 +0530

    Create Example-106.js

---
+++ Java script/Example-106.js
@@ -0,0 +1,16 @@
+//JavaScript: Divide an integer by another integer as long as the result is an integer and return the result
+
+function divide_digit(num, d) {
+       if (d==1)
+         return num;
+       else
+       {
+      while (num % d === 0) {
+        num /= d;
+        }
+    return num;
+       }
+}
+console.log(divide_digit(-12, 2))
+console.log(divide_digit(13, 2))
+console.log(divide_digit(13, 1))
