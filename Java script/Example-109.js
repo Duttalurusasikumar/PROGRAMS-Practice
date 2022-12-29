ba6fe35ee9348798d40233a200508a05693f6d1b
Duttaluru sasikumar <sasi54563@gmail.com>
2022-12-29 08:43:19 +0530

    Create Example-109.js

---
+++ Java script/Example-109.js
@@ -0,0 +1,24 @@
+//JavaScript: Sort an array of all prime numbers between 1 and a given integer
+
+function sort_prime(num) {
+
+  var prime_num1 = [],
+      prime_num2 = [];
+  for (var i = 0; i <= num; i++) {
+    prime_num2.push(true);
+  }
+  for (var i = 2; i <= num; i++) {
+    if (prime_num2[i]) {
+      prime_num1.push(i);
+      for (var j = 1; i * j <= num; j++) {
+        prime_num2[i * j] = false;
+      }
+    }
+  }
+
+  return prime_num1;
+}
+
+console.log(sort_prime(5))
+console.log(sort_prime(11))
+console.log(sort_prime(19))
