5db77ef7726e138ed5dad55f6c9959ece8163372
Duttaluru sasikumar <sasi54563@gmail.com>
2023-01-09 08:29:56 +0530

    Create Example-120.js

---
+++ Java script/Example-120.js
@@ -0,0 +1,13 @@
+//JavaScript: Check whether a point lies strictly inside a given circle
+
+function check_a_point(a, b, x, y, r) {
+    var dist_points = (a - x) * (a - x) + (b - y) * (b - y);
+    r *= r;
+    if (dist_points < r) {
+        return true;
+    }
+    return false;
+}
+
+console.log(check_a_point(0, 0, 2, 4, 6));
+console.log(check_a_point(0, 0, 6, 8, 6));
