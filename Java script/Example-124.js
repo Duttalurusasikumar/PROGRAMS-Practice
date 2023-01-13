5b5381db38331443f1def24bd3be88a7a6dbd31e
Duttaluru sasikumar <sasi54563@gmail.com>
2023-01-13 06:17:41 +0530

    Create Example-124.js

---
+++ Java script/Example-124.js
@@ -0,0 +1,8 @@
+//JavaScript: Create the value of NOR of two given booleans
+
+function test_logical_Nor(a, b) {
+  return (!a && !b)
+}
+console.log(test_logical_Nor(true, false));
+console.log(test_logical_Nor(false, false));
+console.log(test_logical_Nor(true, true));
