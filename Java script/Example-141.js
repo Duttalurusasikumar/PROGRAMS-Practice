32285f146d2bce3045e85d82e7ac6486cb6e3b60
Duttaluru sasikumar <sasi54563@gmail.com>
2023-01-31 13:10:42 +0530

    Create Example-141.js

---
+++ Java script/Example-141.js
@@ -0,0 +1,17 @@
+//JavaScript: Find the number of elements which presents in both of the given arrays
+
+function test_same_elements_both_arrays(arra1, arra2) {
+  var result = 0;
+  for(var i = 0; i < arra1.length; i++) {
+    for(var j = 0; j < arra2.length; j++){
+      if(arra1[i] === arra2[j])
+      {
+        result++;
+      }
+    }
+  }
+  return result;
+}
+console.log(test_same_elements_both_arrays([1,2,3,4], [1,2,3,4]));
+console.log(test_same_elements_both_arrays([1,2,3,4], [1,2,3,5]));
+console.log(test_same_elements_both_arrays([1,2,3,4], [11,22,33,44]));
