37f7b22000432b4355ca94465adbbf7c54ec5424
Duttaluru sasikumar <sasi54563@gmail.com>
2023-02-06 09:55:34 +0530

    Create Example-145.js

---
+++ Java script/Example-145.js
@@ -0,0 +1,12 @@
+//JavaScript: Find the maximum integer n
+
+function sumn(val) {
+  var sn = 0;
+  var i = 0;
+  while (sn <= val) {
+    sn += i++;
+  }
+  return i - 2;
+}
+console.log(sumn(11))
+console.log(sumn(15))
